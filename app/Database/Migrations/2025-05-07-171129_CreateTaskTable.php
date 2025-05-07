de8ab66460a1fd662c5deb62baacdccb8845949e
Muhamad Ibnu Khaidar Hafiz <45196310+Cleign1@users.noreply.github.com>
2025-05-08 00:50:30 +0700

    Create migration for tasks table with fields for title, description, status, position, and timestamps

---
+++ app/Database/Migrations/2025-05-07-171129_CreateTaskTable.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateTaskTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'title' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'description' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+            'status' => [
+                'type' => 'ENUM',
+                'constraint' => ['backlog', 'progress', 'doing', 'review', 'done'],
+                'default' => 'backlog',
+            ],
+            'position' => [
+                'type' => 'INT',
+                'default' => 0,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('tasks');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('tasks');
+    }
+}
