9778b3e553043721d4632ef7e140c4f4302dc65b
Muhamad Ibnu Khaidar Hafiz <45196310+Cleign1@users.noreply.github.com>
2025-05-08 00:50:45 +0700

    Replace HTML structure in about page with a styled main layout and group member list

--- app/Views/pages/about.php
+++ app/Views/pages/about.php
@@ -1,11 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>ini adalah about page</h1>
-</body>
-</html>
+<main class="flex-grow bg-gray-300">
+        <div class="h-full">
+            <div class="container mx-auto px-4 py-8">
+                <h1 class="text-3xl font-bold text-center mb-6">Selamat datang di Halaman About Us</h1>
+                <p class="text-lg text-center mb-4">Berikut adalah Anggota kelompok untuk Aplikasi ini</p>
+                <ul>
+                    <li class="text-lg text-center mb-4">1. Aldo Rizky Ramadhan - 50421106</li>
+                    <li class="text-lg text-center mb-4">2. Faiz Rizki Azmi - 50421454</li>
+                    <li class="text-lg text-center mb-4">3. Muhammad Ibnu Khaidar Hafiz - 50421867</li>
+                    <li class="text-lg text-center mb-4">4. Siti Asma Tomu - 51421427</li>
+                </ul>
+                </div>
+            </div>
+        </div>
+    </main>
