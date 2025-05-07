851f972c977f15ea415d7e4a511af43b9577d5c3
Muhamad Ibnu Khaidar Hafiz <45196310+Cleign1@users.noreply.github.com>
2025-05-08 00:50:19 +0700

    Add kanban routes for managing tasks and positions

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,3 +7,12 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Pages::index');
 $routes->get('/about','Pages::about');
+$routes->group('kanban', function($routes) {
+    $routes->get('/', 'Kanban::index');
+    $routes->get('create', 'Kanban::create');
+    $routes->post('store', 'Kanban::store');
+    $routes->get('edit/(:num)', 'Kanban::edit/$1');
+    $routes->post('edit/(:num)', 'Kanban::update/$1');
+    $routes->get('delete/(:num)', 'Kanban::delete/$1');
+    $routes->post('updatePosition', 'Kanban::updatePosition');
+});
